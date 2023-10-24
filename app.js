4da200efafa675259bcbd29a0625ea27f86ee8b4
Quaviur Rahman <user@Users-MacBook-Pro.local>
2023-10-24 13:09:55 +0600

    added route in app.js and modified vercel.json file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html')}
+  res.sendFile(path.join(__dirname, '/public/index.html'))})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
