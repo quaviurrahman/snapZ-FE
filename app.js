e93b60bc92e43c6ae7636b29531517e219d35e10
Quaviur Rahman <user@Users-MacBook-Pro.local>
2023-10-24 12:20:13 +0600

    added route in app.js and modified vercel.json file

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+const index = require ("./public/index.html")
+const topic = require ("./public/topic.html")
+const post = require ("./public/post.html")
+const dashboard = require ("./public/dashboard.html")
+
 const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 
-app.use(express.static('public'));
+app.use("/",index);
+app.use("/topic",topic);
+app.use("/post",post);
+app.use("/dashboard",dashboard);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
