56c1630962bece79be64787f9ea87e5678c71401
Quaviur Rahman <user@Users-MacBook-Pro.local>
2023-10-27 01:25:32 +0600

    added login and registration page

--- script-index.js
+++ script-index.js
@@ -1,75 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const userRegisterForm = document.getElementById("user-register-form");
+    const userRegisterForm = document.getElementById("user-login-form");
   
-    userRegisterForm.addEventListener("submit", async (e) => {
-      e.preventDefault();
-      const username = document.getElementById("register-username").value;
-      const password = document.getElementById("register-password").value;
-      const email = document.getElementById("register-email").value;
-  
-      // Perform the registration process by making a POST request to your backend
-      registerUser(username, password, email)
-        .then((response) => {
-          if (response.promptCompany) {
-            const company = prompt("Enter your company name:");
-            verifyUser(email, response.otpCode, company)
-              .then((verificationResponse) => {
-                if (verificationResponse) {
-                  console.log("Registration successful");
-                }
-              })
-              .catch((error) => {
-                console.error("Error during verification:", error);
-              });
-          } else {
-            const otpCode = prompt("Enter the OTP code sent to your email:");
-            verifyUser(email, otpCode)
-              .then((verificationResponse) => {
-                if (verificationResponse) {
-                  console.log("Registration successful");
-                }
-              })
-              .catch((error) => {
-                console.error("Error during verification:", error);
-              });
-          }
-        })
-        .catch((error) => {
-          console.error("Error during registration:", error);
-        });
-    });
-  
-    async function registerUser(username, password, email) {
-      try {
-        const response = await fetch("https://snap-z-be.vercel.app/auth/register", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ username, password, email }),
-        });
-        return response.json();
-      } catch (error) {
-        throw error;
-      }
-    }
-  
-    async function verifyUser(email, otpCode, company = null) {
-      try {
-        const response = await fetch("https://snap-z-be.vercel.app/auth/verify", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email, otpCode, company }),
-        });
-        return response.json();
-      } catch (error) {
-        throw error;
-      }
-    }
-
-
   // Function to log in a user
   const loginUser = async (username, password) => {
     try {
@@ -94,15 +25,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  userRegisterForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const username = document.getElementById("register-username").value;
-    const password = document.getElementById("register-password").value;
-    const registered = await registerUser(username, password);
-    if (registered) {
-      // Handle successful registration, e.g., show a success message
-    }
-  });
 
   userLoginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
---
+++ script-register.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const userRegisterForm = document.getElementById("user-register-form");
+  
+    userRegisterForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const username = document.getElementById("register-username").value;
+      const password = document.getElementById("register-password").value;
+      const email = document.getElementById("register-email").value;
+  
+      // Perform the registration process by making a POST request to your backend
+      registerUser(username, password, email)
+        .then((response) => {
+          if (response.promptCompany) {
+            const company = prompt("Enter your company name:");
+            verifyUser(email, response.otpCode, company)
+              .then((verificationResponse) => {
+                if (verificationResponse) {
+                  console.log("Registration successful");
+                }
+              })
+              .catch((error) => {
+                console.error("Error during verification:", error);
+              });
+          } else {
+            const otpCode = prompt("Enter the OTP code sent to your email:");
+            verifyUser(email, otpCode)
+              .then((verificationResponse) => {
+                if (verificationResponse) {
+                  console.log("Registration successful");
+                }
+              })
+              .catch((error) => {
+                console.error("Error during verification:", error);
+              });
+          }
+        })
+        .catch((error) => {
+          console.error("Error during registration:", error);
+        });
+    });
+  
+    async function registerUser(username, password, email) {
+      try {
+        const response = await fetch("https://snap-z-be.vercel.app/auth/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password, email }),
+        });
+        return response.json();
+      } catch (error) {
+        throw error;
+      }
+    }
+  
+    async function verifyUser(email, otpCode, company = null) {
+      try {
+        const response = await fetch("https://snap-z-be.vercel.app/auth/verify", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, otpCode, company }),
+        });
+        return response.json();
+      } catch (error) {
+        throw error;
+      }
+    }
+
+    userRegisterForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const username = document.getElementById("login-username").value;
+        const password = document.getElementById("login-password").value;
+        const registered = await registerUser(username, password);
+        if (registered) {
+          // Handle successful registration, e.g., show a success message
+        }
+      });
+  });
+
+
